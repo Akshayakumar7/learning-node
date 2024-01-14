7f7a5c3a41a9873d8399ce839d086be07760445b
Akshay Kumar <akshaykumar@veenamtech.com>
2024-01-14 11:41:52 +0530

    learned upload file and os module

--- index.js
+++ index.js
@@ -697,6 +697,41 @@
 
 //Events and Event Emitter in Node js
 
+// const express = require("express");
+// const EventEmitter = require("events");
+// const app = express(); 
+// const event = new EventEmitter();
+
+// let count = 0;
+
+// event.on("countAPI", () => {
+//     count++;
+//     console.log("event called", count);
+// });
+
+// app.get("/", (req, resp) => {
+//     resp.send("api called");
+//     event.emit("countAPI");
+// });
+
+// app.get("/search", (req, resp) => {
+//     resp.send("search api called");
+// });
+
+// app.get("/update", (req, resp) => {
+//     resp.send("update api called");
+// });
+
+// app.listen(4000);
+
+
+//----------------------------------------------------------------------------------------------
+
+// REPL (Read Eval Print loop)
+
+
+
+
 
 
 
